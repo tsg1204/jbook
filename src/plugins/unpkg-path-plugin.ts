59abe182530d37f78c173550a7dd30e1081ce65e
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-24 12:57:42 -0500

    tested cache result

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,17 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache'
+});
+
+// (async () => {
+//   await fileCache.setItem('color', 'red');
+
+//   const color = await fileCache.getItem('color');
+//   console.log(color);
+// })()
 
 export const unpkgPathPlugin = () => {
   return {
@@ -37,13 +49,26 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        //Check to see if we have already fetched this file
+        //and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        //if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
         console.log(request);
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+        //store reponse in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
