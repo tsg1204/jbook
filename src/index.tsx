3987a14cf971ded3f6c51e39cacfd6b7cd39c22b
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-26 10:15:15 -0500

    bundle inside the browser

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -35,14 +36,22 @@ const App = () => {
       },
     });
     // console.log(result);
-    setCode(result.outputFiles[0].text);
-
+    //setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
 
   const html = `
-    <script>
-      ${code}
-    </script>
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data);
+          }, false);
+        </script>
+      </body>
+    </html>
   `;
 
   return (
@@ -55,7 +64,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe srcDoc={html} sandbox="allow-scripts" />
+      <iframe ref={iframe} srcDoc={html} sandbox="allow-scripts" />
     </div>
   );
 };
