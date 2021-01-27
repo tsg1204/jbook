496c77a3bf0e7334b438f72f413464999281b1b9
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-26 23:35:22 -0500

    markdown editor adde

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import CodeCell from './components/code-cell';
+//import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const App = () => {
 
   return (
     <div>
-      <CodeCell />
+      {/*<CodeCell />*/}
+      <TextEditor />
       
     </div>
   );
