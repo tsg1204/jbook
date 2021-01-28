acf5512af914011a133be6541ebc55d05a6f7cd1
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-28 10:01:21 -0500

    added useTypedSelector

---
+++ src/components/cell-list-item.tsx
@@ -0,0 +1,8 @@
+
+const CellListItem: React.FC = () => {
+  return (
+    <div>Cell List Item</div>
+  )
+};
+
+export default CellListItem;
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,16 @@ import { Provider } from 'react-redux';
 import { store } from './state';
 //import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 const App = () => {
 
   return (
     <Provider store={store}>
       <div>
-        {/*<CodeCell />*/}
-        <TextEditor />
+        {/*<CodeCell />
+        <TextEditor />*/}
+        <CellList />
       </div>
     </Provider>
   );
