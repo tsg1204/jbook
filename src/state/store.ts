2e70b40ae3f4e761381dc527a464eec80b782595
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-28 10:47:15 -0500

    cell list items manual test

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,28 +1,29 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-//import { ActionType } from './action-types';
+import { ActionType } from './action-types';
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 //manual test
-// store.dispatch({
-//   type: ActionType.INSERT_CELL_BEFORE,
-//   payload: {
-//     id: null,
-//     type: 'code'
-//   }
 
-// });
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code'
+  }
 
-// store.dispatch({
-//   type: ActionType.INSERT_CELL_BEFORE,
-//   payload: {
-//     id: null,
-//     type: 'text'
-//   }
+});
 
-// });
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text'
+  }
+
+});
 
 // const id = store.getState().cells.order[0];
 // console.log(store.getState());
