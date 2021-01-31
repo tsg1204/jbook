b3bb712e97891d2e9cc12c9630c006a16f6849f8
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-31 17:49:56 -0500

    cell list style added

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
+import './cell-list.css';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({cells: {order, data}}) => {
@@ -18,7 +19,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell forceVisible={cells.length === 0} previousCellID={null} />
       {renderedCells}
     </div>
